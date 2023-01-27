565c95205011a18df1ed52b1c24ce417765c8eea
codeeshop-oc/github-action-test <github-actions[bot]@users.noreply.github.com>
2023-01-27 08:39:37 +0000

    Automated deployment

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'prestashop/phpcs',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '54c794d2f4fb2915cfd290ab1df82c2b9cd593a7',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'prestashop/phpcs' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '54c794d2f4fb2915cfd290ab1df82c2b9cd593a7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
